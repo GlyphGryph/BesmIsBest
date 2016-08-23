e4448e73ccb066a37aa5793227af27b6596b3b6c
GlyphGryph <glyphgryph@gmail.com>
2016-08-22 22:23:44 -0400

    Added health and time unit bars

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -9,14 +9,16 @@ class Battle < ApplicationRecord
         image: ActionController::Base.helpers.image_url('feardolon.png'),
         health: 50,
         max_health: 50,
+        health_percent: 100,
         time_units: 19,
         time_unit_percent: 19*100/20
       },
       side_two: {
         name: 'Faithdolon',
         image: ActionController::Base.helpers.image_url('faithdolon.png'),
-        health: 100,
+        health: 75,
         max_health: 100,
+        health_percent: 75,
         time_units: 10,
         time_unit_percent: 50
       }

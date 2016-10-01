f0737e067c2c03df791d2fe3ac5650ee59782675
GlyphGryph <glyphgryph@gmail.com>
2016-09-30 22:54:37 -0400

    Re-randomize starting spirit for now

--- app/models/character.rb
+++ app/models/character.rb
@@ -90,7 +90,7 @@ private
 
   def setup_associations
     team = Team.create!(character: self)
-    starter_spirit = Spirit.create!(species_id: 'rigby')
+    starter_spirit = Spirit.create!(species_id: ['nellaf', 'rigby', 'obsoc'].sample)
     TeamMembership.create!(team: team, spirit: starter_spirit, position: 0)
    end
 end
